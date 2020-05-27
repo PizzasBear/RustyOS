6fce2044ca7224a0d9935d0cfa92542f62cebd55
PizzasBear <mx.sht06@gmail.com>
2020-05-27 07:41:05 +0300

    run.sh rename fixes and Hello Wolrd!

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cargo bootimage --target=tstos.json
-qemu-system-x86_64 -drive format=raw,file="target/tstos/debug/bootimage-rustyos.bin"
+cargo bootimage --target=rustyos.json
+qemu-system-x86_64 -drive format=raw,file="target/rustyos/debug/bootimage-rustyos.bin"
