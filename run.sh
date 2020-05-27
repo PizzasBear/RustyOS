2478d94707899750e2889b4f51dee29043bdbeea
PizzasBear <mx.sht06@gmail.com>
2020-05-27 07:28:50 +0300

    Rename

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cargo bootimage --target=tstos.json
-qemu-system-x86_64 -drive format=raw,file="target/tstos/debug/bootimage-tst.bin"
+qemu-system-x86_64 -drive format=raw,file="target/tstos/debug/bootimage-rustyos.bin"
